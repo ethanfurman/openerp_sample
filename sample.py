707d8811e4a1857527be0bf7a5bd25e43d1bf14e
Ethan Furman <ethan@stoneleaf.us>
2016-04-01 13:16:04 -0700

    refactor _get_address out of onchange methods

--- sample.py
+++ sample.py
@@ -73,19 +73,24 @@ class sample_request(osv.Model):
             }
         }
 
-    def _get_address(self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        if ids:
-            res_partner = self.pool.get('res.partner')
-            for record in self.read(cr, uid, ids, fields=['partner_id']):
-                id = record['id']
-                partner_id = record['partner_id'][0]
-                partner = res_partner.browse(cr, uid, partner_id)
+    def _get_address(self, cr, uid, send_to, user_id, contact_id, partner_id, context=None):
+        res_partner = self.pool.get('res.partner')
+        if send_to == 'rep':
+            # stuff the rep's address into the record
+            user = self.pool.get('res.users').browse(cr, uid, user_id, context=context)
+            rep = user.partner_id
+            label = rep.name + '\n' + res_partner._display_address(cr, uid, rep)
+        elif send_to:
+            label = False
+            if contact_id:
+                contact = res_partner.browse(cr, uid, contact_id, context=context)
+                label = contact.name + '\n' + res_partner._display_address(cr, uid, contact)
+            elif partner_id:
+                partner = res_partner.browse(cr, uid, partner_id, context=context)
                 label = partner.name + '\n' + res_partner._display_address(cr, uid, partner)
-                res[id] = label
-        return res
+        else:
+            label = False
+        return label
 
     def _get_pdf(self, cr, uid, ids, field_name, arg, context=None):
         res = {}
@@ -125,7 +130,7 @@ class sample_request(osv.Model):
         'department': fields.selection([('marketing', 'SAMMA - Marketing'), ('sales', 'SAMSA - Sales')], string='Department', required=True, track_visibility='onchange'),
         'user_id': fields.many2one('res.users', 'Request by', required=True, track_visibility='onchange'),
         'create_date': fields.datetime('Request created on', readonly=True, track_visibility='onchange'),
-        'send_to': fields.selection([('rep', 'Sales Rep in office'), ('address', 'Customer')], string='Send to', required=True, track_visibility='onchange'),
+        'send_to': fields.selection([('rep', 'Sales Rep'), ('customer', 'Customer')], string='Send to', required=True, track_visibility='onchange'),
         'target_date_type': fields.selection([('ship', 'Ship'), ('arrive', 'Arrive')], string='Samples must', required=True, track_visibility='onchange'),
         'target_date': fields.date('Target Date', required=True, track_visibility='onchange'),
         'instructions': fields.text('Special Instructions', track_visibility='onchange'),
@@ -134,7 +139,7 @@ class sample_request(osv.Model):
         'contact_id': fields.many2one('res.partner', 'Contact', track_visibility='onchange'),
         'contact_name': fields.related('contact_id', 'name', type='char', size=64, string='Contact Name'),
         # fields needed for shipping
-        'address': fields.function(_get_address, type='text', string='Shipping Label', track_visibility='onchange'),
+        'address': fields.text(string='Shipping Label'),
         'address_type': fields.selection([('business', 'Commercial'), ('personal', 'Residential')], string='Address type', required=True, track_visibility='onchange'),
         'ice': fields.boolean('Add ice', track_visibility='onchange'),
         'request_ship': fields.selection(REQUEST_SHIPPING, string='Ship Via', required=True, track_visibility='onchange'),
@@ -205,37 +210,40 @@ class sample_request(osv.Model):
             res.append((record.id, name))
         return res
 
-    def onchange_contact_id(self, cr, uid, ids, contact_id, partner_id, send_to, context=None):
+    def onchange_contact_id(self, cr, uid, ids, send_to, user_id, contact_id, partner_id, context=None):
         res = {'value': {}, 'domain': {}}
         if contact_id:
             res_partner = self.pool.get('res.partner')
             contact = res_partner.browse(cr, uid, contact_id)
-            if partner_id and send_to != 'rep':
-                res['value']['address'] = contact.name + '\n' + res_partner._display_address(cr, uid, contact)
+            if contact.is_company:
+                # move into the partner_id field
+                res['value']['partner_id'] = contact_id
+                res['value']['contact_id'] = False
+                res['domain']['contact_id'] = [('parent_id','=',contact.id)]
+            elif contact.parent_id:
+                # set the partner_id field with this parent
+                res['value']['partner_id'] = contact.parent_id.id
+                res['domain']['contact_id'] = [('parent_id','=',contact.parent_id.id)]
             else:
-                if contact.is_company:
-                    # move into the partner_id field
-                    res['value']['partner_id'] = contact_id
-                    res['value']['contact_id'] = False
-                    res['domain']['contact_id'] = [('parent_id','=',contact.id)]
-                elif contact.parent_id:
-                    # set the partner_id field with this parent
-                    res['value']['partner_id'] = contact.parent_id.id
-                    res['domain']['contact_id'] = [('parent_id','=',contact.parent_id.id)]
-                else:
-                    # non-company person; shove value into partner
-                    res['value']['partner_id'] = contact.id
-                    res['value']['contact_id'] = False
-                    res['domain']['contact_id'] = []
+                # non-company person; shove value into partner
+                res['value']['partner_id'] = contact.id
+                res['value']['contact_id'] = False
+                res['domain']['contact_id'] = []
+        res['value']['address'] = self._get_address(cr, uid, send_to, user_id, contact_id, partner_id, context=context)
         return res
 
-    def onchange_partner_id(self, cr, uid, ids, partner_id, contact_id, send_to, context=None):
+    def onchange_partner_id(self, cr, uid, ids, send_to, user_id, contact_id, partner_id, context=None):
+        print 'sample.onchange_partner_id'
+        for name, value in (
+                ('self', self), ('cr', cr), ('uid', uid), ('ids', ids),
+                ('send_to', send_to), ('user_id', user_id), ('contact_id', contact_id),
+                ('partner_id', partner_id), ('context', context),
+                ):
+            print name, value
         res = {'value': {}, 'domain': {}}
         if not partner_id:
             res['value']['contact_id'] = False
             res['domain']['contact_id'] = []
-            if send_to != 'rep':
-                res['value']['address'] = False
         else:
             res_partner = self.pool.get('res.partner')
             partner = res_partner.browse(cr, uid, partner_id)
@@ -260,32 +268,16 @@ class sample_request(osv.Model):
                 res['domain']['contact_id'] = []
                 res['invisible']['contact_id'] = True
 
-            if send_to != 'rep':
-                if contact_id:
-                    label = contact.name + '\n' + res_partner._display_address(cr, uid, contact)
-                elif partner_id:
-                    label = partner.name + '\n' + res_partner._display_address(cr, uid, partner)
-                res['value']['address'] = label
+        res['value']['address'] = self._get_address(cr, uid, send_to, user_id, contact_id, partner_id, context=context)
         return res
 
     def onchange_send_to(self, cr, uid, ids, send_to, user_id, contact_id, partner_id, context=None):
         res = {'value': {}, 'domain': {}}
-        res_partner = self.pool.get('res.partner')
-        if send_to == 'rep':
-            # stuff the rep's address into the record
-            user = self.pool.get('res.users').browse(cr, uid, user_id, context=context)
-            rep = user.partner_id
-            res['value']['address'] = rep.name + '\n' + res_partner._display_address(cr, uid, rep)
-        elif send_to != 'rep':
-            label = False
-            if contact_id:
-                contact = res_partner.browse(cr, uid, contact_id, context=context)
-                label = contact.name + '\n' + res_partner._display_address(cr, uid, contact)
-            elif partner_id:
-                partner = res_partner.browse(cr, uid, partner_id, context=context)
-                label = partner.name + '\n' + res_partner._display_address(cr, uid, partner)
-            res['value']['address'] = label
+        res['value']['address'] = self._get_address(cr, uid, send_to, user_id, contact_id, partner_id, context=context)
         return res
+
+    def onload(self, cr, uid, ids, send_to, user_id, contact_id, partner_id, context=None):
+        return self.onchange_partner_id(cr, uid, ids, send_to, user_id, contact_id, partner_id, context=context)
 
     def write(self, cr, uid, ids, values, context=None):
         if ids:
