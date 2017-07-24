ffcb6d8e12c18b366f01e92899c63519889096f2
Ethan Furman <ethan@stoneleaf.us>
2017-07-24 03:22:23 -0700

    enable searching by name/fis-id (fal-i42)

--- sample.py
+++ sample.py
@@ -512,6 +512,7 @@ class sample_product(osv.Model):
     _name = 'sample.product'
 
     _columns = {
+        'name': fields.related('product_id', 'name'),
         'request_id': fields.many2one('sample.request', string='Request'),
         'request_state': fields.related('request_id','state', type='char'),
         'qty_id': fields.many2one('sample.qty_label', string='Qty', oldname='qty'),
